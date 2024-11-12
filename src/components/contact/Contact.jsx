8191a33b5b1d610cb1749822cb1970cb845bb6fd
Scott Larson <154692190+larsonscott89@users.noreply.github.com>
2024-11-11 19:15:14 -0500

    form logic done

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,18 @@
 import styles from './ContactStyles.module.css'
+import { useForm, ValidationError } from '@formspree/react'
 
 function Contact() {
+
+  window.onbeforeunload = () => {
+    for(const form of document.getElementsByTagName('form')) {
+      form.reset();
+    }
+  }
+
   return (
     <section id='contact' className={styles.container}>
       <h1 className='sectionTitle'>Contact</h1>
-      <form action=''>
+      <form action='https://formspree.io/f/xovqwweo' method='POST' >
         <div className='formGroup'>
           <label htmlFor='name' hidden>
             Name
